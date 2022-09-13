1a27f8d696b374a1fd5588f61cf235a8d334ff45
Touko Päivänsalo <touko.paivansalo@gmail.com>
2022-09-13 16:35:52 +0300

    Add suggested plan functionality

--- components/Info.js
+++ components/Info.js
@@ -3,10 +3,12 @@ import styles from '../styles/BulletPoints.module.css'
 
 export default function BulletPoints() {
   const bulletPoints = useStore(state => state.info);
+  const energyType = useStore(state => state.energyType);
 
-  return (
+  return (<>
     <ul className={styles.list}>
+      <h2>Facts about {energyType} power:</h2>
       {bulletPoints.map((point, i) => <li key={i} >{point}</li>)}
     </ul>
-  );
+  </>);
 }
--- components/SuggestedPlan.js
+++ components/SuggestedPlan.js
@@ -16,9 +16,34 @@ export function SuggestedPlan() {
   const suggestedPlan = useStore(state => state.suggestedPlan);
 
   switch (suggestedPlan) {
+    case 'constant':
+      return <Plan
+        name="Ilmasto Vakio"
+        description="Ilmasto Vakio soveltuu kohteisiin, joissa
+        vuosikulutus on alle 2 500 kWh, kuten
+        esimerkiksi kerrostaloasuntoihin.
+        Kiinteän kuukausihinnan vuoksi tiedät
+        sähkölaskusi suuruuden ennakolta.
+        Sähkö on tuotettu fossiilivapaan Ilmasto
+        Mixin mukaisesti vesi-, tuuli- ja
+        ydinvoimalla."
+        price="Kiinteä kuukausihinta: 25 EUR/kk"
+      />;
+    case 'kvartal':
+      return <Plan
+        name="Ilmasto/Täysvesi Kvartaali"
+        description="Vaivaton sähkösopimus sopii kaikkiin koteihin
+        ja pienyrityksiin. Sopimus on voimassa
+        toistaiseksi ja sähkön hinta tarkistetaan
+        neljästi vuodessa kvartaaleittain. Sopimuksen
+        sähkö on tuotettu fossiilivapaasti vesi-, tuuli-
+        ja ydinvoimalla"
+        price="Energia: 12,70 snt/kWh Perusmaksu: 3,50 EUR/kk"
+      />;
     default:
-      return <Plan name="Ilmasto Mix 24kk Täysvesi, Täysaurinko,
-      Täysydin, Täystuuli" description="Määräaikainen -sopimus sopii kaikkiin
+      return <Plan
+        name="Ilmasto Mix 24kk Täysvesi, Täysaurinko, Täysydin, Täystuuli"
+        description="Määräaikainen -sopimus sopii kaikkiin
         koteihin ja pienyrityksiin. Sähkön hinta pysyy
         samana koko sopimuskauden ajan ja
         lähestymme sinua uusintatarjouksella ennen
