221038b91f5c5d0a54160a266c6f231714a2bd00
Touko Päivänsalo <touko.paivansalo@gmail.com>
2022-09-13 11:29:23 +0300

    Add chart sections

---
+++ components/ChartSection.js
@@ -0,0 +1,14 @@
+import useStore from "../lib/store";
+import HorizontalBar from '../components/horizontalBar';
+
+export default function ChartSection() {
+  const co2emissions = useStore(state => state.co2emissions);
+  const price = useStore(state => state.price);
+
+  return (<>
+    <HorizontalBar label={(
+      <>CO<sup>2</sup> Emissions</>
+    )} sliderValue={co2emissions} max={100} />
+    <HorizontalBar label="Price" sliderValue={price} max={10} />
+  </>)
+}
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,22 @@
 import ParallaxEffect from '../components/parallaxEffect';
 import MultipleChoice from '../components/multipleChoice';
 import GraphSection from '../components/GraphSection';
+import ChartSection from '../components/ChartSection';
 
 
 export default function Home() {
   
   return (
     <ParallaxEffect heading="Parallax effect">
-    <GraphSection />
+      <GraphSection />
       
       <MultipleChoice buttons={[
         { text: 'Aurinko Energia', value: 'solar' },
         { text: 'Tuulivoima', value: 'wind' },
       ]} />
     
+      <ChartSection />
+
     </ParallaxEffect>
   )
 }
