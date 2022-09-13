9065866a0f0daf9e1b6643877aa163d303ea4bb7
vsuominen <93792963+vsuominen@users.noreply.github.com>
2022-09-13 09:49:19 +0300

    slider working and removed slider from parallax_effect.html

--- components/horizontalBar.js
+++ components/horizontalBar.js
@@ -1,14 +1,14 @@
 import styles from '../styles/HorizontalBar.module.css';
 
-export default function HorizontalBar({ label, value, max, unit }) {
+export default function HorizontalBar({ label, sliderValue, max, unit }) {
   return (
     <div className={styles.container}>
       <div>
-        <p className={styles.label}>{label}: {value} {unit}</p>
+        <p className={styles.label}>{label}: {sliderValue} {unit}</p>
       </div>
       <div className={styles.barBackground}>
         <div className={styles.bar} style={{
-          width: ((value * 100.0) / max)+'%'
+          width: ((sliderValue * 100.0) / max)+'%'
         }} />
       </div>
     </div>
