173509ef77fa9bdfe73e9b75612725bc958eab1e
vsuominen <93792963+vsuominen@users.noreply.github.com>
2022-09-13 14:58:05 +0300

    slider min value set to 15 and bar corners rounded

--- components/slider.js
+++ components/slider.js
@@ -5,7 +5,7 @@ export default function({ setRangevalue, max }) {
 
     return (
         <div className={styles.container}>
-            <input className={styles.slider} type="range" min={0} max={max} onInput={(event) => setRangevalue(event.target.value)} />
+            <input className={styles.slider} type="range" min={15} max={max} onInput={(event) => setRangevalue(event.target.value)} />
         </div>
     );
 };
